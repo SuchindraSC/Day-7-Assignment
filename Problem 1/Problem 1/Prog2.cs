20e58dde7d7c49f60b828c7aa983c3ea2d4bc145
SuchindraSC <suchindrachitnis@gmail.com>
2021-08-08 18:15:37 +0530

    Day 7 OOPS - 1

---
+++ Problem 1/Problem 1/Prog2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problem_1
+{
+    class Prog2
+    {
+        static void Main(string[] args)
+        {
+
+            LineComparison myObj = new LineComparison();
+            Console.WriteLine("The Length of Line is "+myObj.LengthOfLine);
+        }
+    }
+}
---
+++ Problem 1/Problem 1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Problem_1
+{
+     public class LineComparison
+    {
+            static int x1 = Convert.ToInt32(Console.ReadLine());
+            static int x2 = Convert.ToInt32(Console.ReadLine());
+            static int y1 = Convert.ToInt32(Console.ReadLine());
+            static int y2 = Convert.ToInt32(Console.ReadLine());
+            public double LengthOfLine = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+    }
+}
